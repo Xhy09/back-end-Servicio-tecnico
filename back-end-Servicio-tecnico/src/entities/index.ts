c0bccc0d023e5dbd18187e32eccbdcebb180c6b1
Josué David Silvestre Martinez <133934618+Xhy09@users.noreply.github.com>
2025-10-23 15:11:44 -0600

    borrando directorio mal subido

--- back-end-Servicio-tecnico/src/entities/index.ts
+++
@@ -1,9 +0,0 @@
-// Exportar todas las entidades desde un solo archivo
-export { User, UserRole, UserStatus } from './user.entity';
-export { Product, Category } from './product.entity';
-export { ProductImage } from './product-image.entity';
-export { Quotation, QuotationStatus } from './quotation.entity';
-export { QuotationItem } from './quotation-item.entity';
-export { Service, ServiceStatus, ServicePriority } from './service.entity';
-export { ServiceImage } from './service-image.entity';
-export { AuditLog, AuditAction } from './audit-log.entity';
